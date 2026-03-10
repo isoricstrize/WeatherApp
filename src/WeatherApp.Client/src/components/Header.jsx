52aa83041f2dbfd9b4ab9d32b821cd6017614fd4
Ivana Soric Strize <ivana.soric2@gmail.com>
2026-03-10 14:14:34 +0100

    Minor fixes.

--- src/WeatherApp.Client/src/components/Header.jsx
+++ src/WeatherApp.Client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <h1>Simple Weather App</h1>
+      <h1>Weather Forecast</h1>
       <p>Weather data provided by OpenWeatherMap</p>
     </header>
   );
